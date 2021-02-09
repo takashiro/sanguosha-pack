099ac79bd29370b3cd6265adf2a735c04ba41961
Kazuichi Takashiro <takashiro@qq.com>
2021-02-09 15:08:23 +0800

    Add subtypes of equip card

---
+++ src/quick/HorseCard.ts
@@ -0,0 +1,6 @@
+import EquipCard from './EquipCard';
+
+abstract class HorseCard extends EquipCard {
+}
+
+export default HorseCard;
--- src/quick/index.ts
+++ src/quick/index.ts
@@ -5,3 +5,10 @@ export { default as SkillEffect } from './SkillEffect';
 export { default as BasicCard } from './BasicCard';
 export { default as EquipCard } from './EquipCard';
 export { default as TrickCard } from './TrickCard';
+
+export { default as WeaponCard } from './WeaponCard';
+export { default as ArmorCard } from './ArmorCard';
+export { default as HorseCard } from './HorseCard';
+export { default as DefensiveHorseCard } from './DefensiveHorseCard';
+export { default as OffensiveHorseCard } from './OffensiveHorseCard';
+export { default as TreasureCard } from './TreasureCard';
